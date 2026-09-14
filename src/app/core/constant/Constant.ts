85836dfc95fce90a7cb810596e1d453e06d886cd
Priya Tapkire <priyatapkire27@gmail.com>
2026-09-14 20:22:23 +0530

    Static data into the Global constant

--- src/app/core/constant/Constant.ts
+++ src/app/core/constant/Constant.ts
@@ -2,5 +2,9 @@ export const GlobalConstant = {
      API_ENDPOINTS: {
         LOGIN : 'login',
         GET_USER_BY_ID : 'getUserbyId?id='
+     },
+     VALIDATION_MESSAGES:{
+        REQUIRED : 'This is required',
+        MOBILE_NO : 'Max and Min 10 Char Needed'
      }
 };
