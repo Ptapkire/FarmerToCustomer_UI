a11dfaddc2deb87e7707d569ba80398361961095
Priya Tapkire <priyatapkire27@gmail.com>
2026-09-15 20:42:02 +0530

    comments

--- src/app/core/constant/Constant.ts
+++ src/app/core/constant/Constant.ts
@@ -3,6 +3,7 @@ export const GlobalConstant = {
         LOGIN : 'login',
         GET_USER_BY_ID : 'getUserbyId?id='
      },
+     // Static messages basically using for forms
      VALIDATION_MESSAGES:{
         REQUIRED : 'This is required',
         MOBILE_NO : 'Max and Min 10 Char Needed'
